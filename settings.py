76699f456d81e9c4335f5655130bfd2586d552dd
Alexitachi <pranshu09876@gmail.com>
2020-02-24 11:53:50 +0530

    Ready for deployment to heroku.

--- settings.py
+++ settings.py
@@ -102,9 +102,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
 # My settings
 LOGIN_URL = '/users/login/'
 
